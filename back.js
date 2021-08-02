643b07fc20a3b805beb1d57a3f10f18ce1af3dc3
omkar patke <omkarpatke1101@gmail.com>
2021-08-02 17:47:37 +0530

    add : links

---
+++ back.js
@@ -0,0 +1,23 @@
+var Input = document.querySelector(".input");
+var Cheakbtn = document.querySelector(".cheakbtn");
+var OutputDiv = document.querySelector(".output");
+
+
+
+
+Cheakbtn.addEventListener("click" ,clickHandler);
+
+function clickHandler(){
+    const value = Input.value;
+    var date = value.split("-");
+    const inputYear = date[0];
+    const inputMonth = date[1];
+    const inputDate = date[2];
+    
+   
+if(date){
+    
+}
+    
+   
+}
