9644275f07b84598ecced671cb6f338d6f2953ba
omkar patke <omkarpatke1101@gmail.com>
2021-08-14 17:10:43 +0530

    add : css

--- back.js
+++ back.js
@@ -1,7 +1,7 @@
 var Input = document.querySelector(".input");
 var Cheakbtn = document.querySelector(".cheakbtn");
 var OutputDiv = document.querySelector(".output");
-
+var Outputimg = document.querySelector(".img");
 
 
 
@@ -10,14 +10,6 @@ Cheakbtn.addEventListener("click" ,clickHandler);
 function clickHandler(){
     const value = Input.value;
     var date = value.split("-");
-    const inputYear = date[0];
-    const inputMonth = date[1];
-    const inputDate = date[2];
-    
-   
-if(date){
-    
 }
-    
    
-}
+
